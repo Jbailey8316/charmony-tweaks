be169a7cc486d39857cfd510abd0b873bb506b67
Jeff Bailey <j.bailey8316@att.net>
2026-08-08 10:10:01 -0400

    Add variant-specific Moobloom heads

--- src/main/java/svenhjol/charmony/tweaks/common/features/mob_drops/mobs/MoobloomHeadDrops.java
+++ src/main/java/svenhjol/charmony/tweaks/common/features/mob_drops/mobs/MoobloomHeadDrops.java
@@ -1,33 +1,22 @@
 package svenhjol.charmony.tweaks.common.features.mob_drops.mobs;
 
-import com.mojang.authlib.GameProfile;
-import com.mojang.authlib.properties.Property;
-import net.minecraft.core.component.DataComponents;
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.Items;
-import net.minecraft.world.item.component.ResolvableProfile;
 import svenhjol.charmony.core.base.Setup;
 import svenhjol.charmony.core.helpers.EnchantmentsHelper;
+import svenhjol.charmony.mooblooms.common.features.mooblooms.Moobloom;
 import svenhjol.charmony.tweaks.common.features.mob_drops.DropProvider;
 import svenhjol.charmony.tweaks.common.features.mob_drops.MobDrops;
 
-import java.nio.charset.StandardCharsets;
 import java.util.Optional;
-import java.util.UUID;
 
 public class MoobloomHeadDrops extends Setup<MobDrops> implements DropProvider {
     static final double BASE_DROP_CHANCE = 0.01d;
     static final double LOOTING_BONUS = 0.001d;
-
-    private static final ResourceLocation MOOBLOOM = ResourceLocation.fromNamespaceAndPath("charmony", "moobloom");
-    private static final String TEXTURE = "eyJ0ZXh0dXJlcyI6eyJTS0lOIjp7InVybCI6Imh0dHA6Ly90ZXh0dXJlcy5taW5lY3JhZnQubmV0L3RleHR1cmUvYjc5ZDdmYzJhYTRjM2VmM2FjNGI4N2Q3MzUxYzM4ZTFjZWJmZGY3NWIwMDJiZDlmNTMwYzA2ODI3ZTUzYWVkNCJ9fX0=";
-    private static final UUID PROFILE_ID = UUID.nameUUIDFromBytes("charmony:moobloom_head".getBytes(StandardCharsets.UTF_8));
 
     public MoobloomHeadDrops(MobDrops feature) {
         super(feature);
@@ -35,7 +24,7 @@ public class MoobloomHeadDrops extends Setup<MobDrops> implements DropProvider {
 
     @Override
     public Optional<ItemStack> dropWhenKilled(LivingEntity entity, DamageSource source) {
-        if (!BuiltInRegistries.ENTITY_TYPE.getKey(entity.getType()).equals(MOOBLOOM)
+        if (!(entity instanceof Moobloom moobloom)
             || !(source.getEntity() instanceof ServerPlayer)
         ) {
             return Optional.empty();
@@ -46,12 +35,8 @@ public class MoobloomHeadDrops extends Setup<MobDrops> implements DropProvider {
             return Optional.empty();
         }
 
-        var profile = new GameProfile(PROFILE_ID, "Moobloom");
-        profile.properties().put("textures", new Property("textures", TEXTURE));
-
-        var head = new ItemStack(Items.PLAYER_HEAD);
-        head.set(DataComponents.PROFILE, ResolvableProfile.createResolved(profile));
-        head.set(DataComponents.CUSTOM_NAME, Component.literal("Moobloom Head"));
-        return Optional.of(head);
+        var headId = ResourceLocation.fromNamespaceAndPath(
+            "charmony", moobloom.getMoobloomType().getName() + "_moobloom_head");
+        return Optional.of(new ItemStack(BuiltInRegistries.ITEM.getValue(headId)));
     }
 }
