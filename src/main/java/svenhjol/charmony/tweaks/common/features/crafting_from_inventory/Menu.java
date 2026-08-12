0824bcf5d4acd01aa20d4af4a6fb62a6b30077b8
Jeff Bailey <j.bailey8316@att.net>
2026-08-11 21:50:15 -0400

    Fix portable crafting result invalidation

--- src/main/java/svenhjol/charmony/tweaks/common/features/crafting_from_inventory/Menu.java
+++ src/main/java/svenhjol/charmony/tweaks/common/features/crafting_from_inventory/Menu.java
@@ -2,8 +2,10 @@ package svenhjol.charmony.tweaks.common.features.crafting_from_inventory;
 
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.inventory.ContainerLevelAccess;
 import net.minecraft.world.inventory.CraftingMenu;
+import net.minecraft.world.Container;
 
 public final class Menu extends CraftingMenu {
     public Menu(int syncId, Inventory inventory, ContainerLevelAccess access) {
@@ -13,5 +15,12 @@ public final class Menu extends CraftingMenu {
     @Override
     public boolean stillValid(Player player) {
         return CraftingFromInventory.feature().handlers.hasCraftingTable(player);
+    }
+
+    @Override
+    public void slotsChanged(Container container) {
+        if (owner().level() instanceof ServerLevel serverLevel) {
+            slotChangedCraftingGrid(this, serverLevel, owner(), craftSlots, resultSlots, null);
+        }
     }
 }
